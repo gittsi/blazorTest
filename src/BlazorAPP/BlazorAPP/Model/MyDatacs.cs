9aa48a4a8c764789dcf9106347b394c11a52a049
Panagiotis Panagiotopoulos <p.panagiotopoulos@kaizengaming.com>
2020-11-08 19:21:49 +0200

    chn

---
+++ src/BlazorAPP/BlazorAPP/Model/MyDatacs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAPP.Model
+{
+    public class MyDatacs
+    {
+        [Required]
+        
+        //[MaxLength(2, ErrorMessage = "number is too long.")]  
+        
+        public int TheNumber { get; set; }
+
+    }
+}
